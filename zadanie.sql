3666f22d77bbe4a8157b7e4bb93f1006155d3573
jkn1k <xkubus2006@gmail.com>
2024-10-08 15:20:08 +0200

    Added changes to zadanie.sql

--- zadanie.sql
+++ zadanie.sql
@@ -10,42 +10,43 @@ CREATE TABLE ksiazki (
     cena DECIMAL(10, 2) NOT NULL
 );
 
-INSERT INTO ksiazki (tytul, autor, rok_wydania, cena) VALUES
-('Książka 1', 'Autor abc', 1985, 139.99),
-('Książka 2', 'Autor xyz', 1995, 349.99),
-('Książka 3', 'Autor qwe', 2005, 429.99);
-
-SELECT * FROM ksiazki;
-
-SELECT * FROM ksiazki WHERE rok_wydania > 1990;
-
-UPDATE ksiazki SET cena = 324.99 WHERE tytul = 'Książka 3';
-
-DELETE FROM ksiazki WHERE tytul = 'Książka 3';
-
--- Zadanie 3
+ALTER TABLE ksiazki
+ADD COLUMN gatunek VARCHAR(100),
+ADD COLUMN isbn VARCHAR(20),
+ADD COLUMN opis TEXT;
 
 CREATE TABLE czytelnicy (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    imie VARCHAR(100),
-    nazwisko VARCHAR(100),
+    imie VARCHAR(100) NOT NULL,
+    nazwisko VARCHAR(100) NOT NULL,
     data_rejestracji DATE
 );
 
 CREATE TABLE wypozyczenia (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    czytelnik_id INT,
-    ksiazka_id INT,
-    data_wypozyczenia DATE,
-    data_zwrotu DATE,
+    czytelnik_id INT NOT NULL,
+    ksiazka_id INT NOT NULL,
+    data_wypozyczenia DATE NOT NULL,
+    data_zwrotu DATE NOT NULL,
     FOREIGN KEY (czytelnik_id) REFERENCES czytelnicy(id),
     FOREIGN KEY (ksiazka_id) REFERENCES ksiazki(id)
 );
 
-ALTER TABLE ksiazki
-ADD COLUMN gatunek VARCHAR(100),
-ADD COLUMN isbn VARCHAR(20),
-ADD COLUMN opis TEXT;
+INSERT INTO ksiazki (tytul, autor, rok_wydania, cena) VALUES
+('Książka 1', 'Autor abc', 1985, 139.99),
+('Książka 2', 'Autor xyz', 1995, 349.99),
+('Książka 3', 'Autor qwe', 2005, 429.99);
+
+INSERT INTO czytelnicy (imie, nazwisko, data_rejestracji) VALUES
+('Adam', 'Kowalski', 2010-10-29),
+('Karol', 'Wiśniewski', 2020-12-13),
+('Jan', 'Nowak', 2008-05-10),
+
+INSERT INTO wypozyczenia (czytelnik_id, ksiazka_id, data_wypozyczenia, data_zwrotu) VALUES
+(1, 1, 2012-08-03, 2012-12-23),
+(2, 2, 2020-12-13, 2023-03-01),
+(3, 3, 2009-05-05, 2011-05-05);
+
 
 CREATE INDEX idx_autor ON ksiazki(autor);
 
