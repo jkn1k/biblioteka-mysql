5e9ac16a1ff81c52b2987ae313aef2230db673f8
jkn1k <xkubus2006@gmail.com>
2024-10-14 14:23:31 +0200

    Added changes to index.html and script.js

--- kalkulator/script.js
+++ kalkulator/script.js
@@ -2,21 +2,30 @@ document.getElementById('calculator').addEventListener('submit', function(event)
 
     event.preventDefault();
 
-    let liczba1 = parseFloat(document.getElementById('liczba1').value);
-    let liczba2 = parseFloat(document.getElementById('liczba2').value);
-    let operacja = document.getElementById('operacja').value;
+    const liczba1 = parseFloat(document.getElementById('liczba1').value);
+    const liczba2 = parseFloat(document.getElementById('liczba2').value);
+    const operacja = document.getElementById('operacja').value;
 
     let wynik;
 
-    if (operacja === 'dodawanie') {
-        wynik = liczba1 + liczba2;
-    } else if (operacja === 'odejmowanie') {
-        wynik = liczba1 - liczba2;
-    } else if (operacja === 'mnozenie') {
-        wynik = liczba1 * liczba2;
-    } else if (operacja === 'dzielenie') {
-        wynik = liczba1 / liczba2;
+    switch (operacja) {
+        case 'dodawanie':
+            wynik = liczba1 + liczba2;
+            break;
+        case 'odejmowanie':
+            wynik = liczba1 - liczba2;
+            break;
+        case 'mnozenie':
+            wynik = liczba1 * liczba2;
+            break;
+        case 'dzielenie':
+            if (liczba2 !== 0) {
+                wynik = liczba1 / liczba2;
+            } else {
+                wynik = 'Nie można dzielić przez zero!';
+            }
     }
+    
 
-    document.getElementById('wynik').textContent = `Wynik: ${wynik}`;
+    document.getElementById('wynik').innerText = `${wynik}`;
 })
