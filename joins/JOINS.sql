ec0e9d8d7718ef82372e86e84846f3ca4bf41355
jkn1k <xkubus2006@gmail.com>
2024-10-28 11:51:53 +0100

    Added missing parts of task

--- joins/JOINS.sql
+++ joins/JOINS.sql
@@ -21,6 +21,24 @@ SELECT U.imie, U.nazwisko, O.ocena
 FROM Uczniowie U
 LEFT JOIN Oceny O ON U.id_ucznia = O.id_ucznia;
 
+SELECT O.ocena, U.imie, U.nazwisko
+FROM Uczniowie U
+RIGHT JOIN Oceny O ON U.id_ucznia = O.id_ucznia;
+
+SELECT U.imie, U.nazwisko, O.ocena 
+FROM Uczniowie U
+LEFT JOIN Oceny O ON U.id_ucznia = O.id_ucznia
+UNION
 SELECT U.imie, U.nazwisko, O.ocena 
 FROM Uczniowie U
 RIGHT JOIN Oceny O ON U.id_ucznia = O.id_ucznia;
+
+SELECT U.imie, U.nazwisko, O.ocena 
+FROM Uczniowie U
+INNER JOIN Oceny O ON U.id_ucznia = O.id_ucznia
+WHERE O.ocena > 4;
+
+SELECT U.imie, U.nazwisko, O.ocena 
+FROM Uczniowie U
+INNER JOIN Oceny O ON U.id_ucznia = O.id_ucznia
+WHERE O.przedmiot = 'Matematyka';
